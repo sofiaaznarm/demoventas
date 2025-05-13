4926c205620e11f8bcb265837fef7b6b94b710c3
sofiaaznarm <sofiaaznarmm@gmail.com>
2025-05-12 18:47:15 -0600

    Update OMS.py

--- OMS.py
+++ OMS.py
@@ -8,3 +8,24 @@ print(df.head())
 import streamlit as st
 
 st.write(df)
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+# Lee el archivo CSV
+df = pd.read_csv('RELAY_WHS.csv')
+
+# Filtra el DataFrame para obtener solo las columnas deseadas y el indicador
+filtered_df = df[(df['Indicator'] == 'Healthy life expectancy at birth') &
+                 (df['Dim1'].isin(['Female', 'Male', 'Total']))]
+
+
+# Crea la gráfica de líneas
+fig = px.line(filtered_df,
+              x='Year',
+              y='Value',
+              color='Dim1',
+              title='Healthy Life Expectancy at Birth')
+
+# Muestra la gráfica en Streamlit
+st.plotly_chart(fig)
