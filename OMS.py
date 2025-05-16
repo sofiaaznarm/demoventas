ca1ba8130607e50ae78c80b92a858bb109738bf8
sofiaaznarm <sofiaaznarmm@gmail.com>
2025-05-15 18:41:52 -0600

    Update OMS.py

--- OMS.py
+++ OMS.py
@@ -1,12 +1,3 @@
-# Lee el archivo CSV
-df = pd.read_csv('RELAY_WHS.csv')
-
-# Muestra las primeras filas del DataFrame para verificar
-print(df.head())
-import streamlit as st
-
-st.write(df)
-
 import pandas as pd
 import altair as alt
 
